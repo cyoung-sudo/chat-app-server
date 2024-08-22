3cef3cead188a2f538a580fd3e7c07faca78cf3d
Christopher Young <christopheryoung@MacBook123.local>
2024-08-22 16:45:21 -0700

    delete all messages if max reached

--- server.js
+++ server.js
@@ -22,18 +22,32 @@ const io = new Server(server, {
 });
 
 io.on("connect", socket => {
-  console.log(`User${socket.id} joined`);
+  console.log(`User(${socket.id}) joined`);
 
   //----- Send messages on initial connection
+  // Delete all messages in max reached
   Message.find({})
     .then(docs => {
-      socket.emit("update", docs);
+      // Check if max reached
+      if(docs.length > 50) {
+        // Delete all
+        Message.deleteMany({})
+          .then(delCount => {
+            return Message.find({});
+          })
+          .then(docs => {
+            io.socket.emit("update", docs);
+          })
+          .catch(err => console.log(err));
+      } else {
+        socket.emit("update", docs);
+      }
     })
     .catch(err => console.log(err));
 
   //----- Handle new messages
   socket.on("message", data => {
-    console.log(`User(${socket.id}: ${data})`);
+    console.log(`User(${socket.id}): ${data})`);
     // Create/Save message
     Message.create({
       user: `User(${socket.id})`,
