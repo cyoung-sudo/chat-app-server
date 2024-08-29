23aca4ba6dd46cb486fb5ed8057d15c05488a878
Christopher Young <christopheryoung@MacBook123.local>
2024-08-28 21:54:46 -0700

    minor changes

--- server.js
+++ server.js
@@ -22,32 +22,16 @@ const io = new Server(server, {
 });
 
 io.on("connect", socket => {
-  console.log(`User(${socket.id}) joined`);
-
   //----- Send messages on initial connection
   // Delete all messages in max reached
   Message.find({})
     .then(docs => {
-      // Check if max reached
-      if(docs.length > 50) {
-        // Delete all
-        Message.deleteMany({})
-          .then(delCount => {
-            return Message.find({});
-          })
-          .then(docs => {
-            io.socket.emit("update", docs);
-          })
-          .catch(err => console.log(err));
-      } else {
-        socket.emit("update", docs);
-      }
+      socket.emit("update", docs);
     })
     .catch(err => console.log(err));
 
   //----- Handle new messages
   socket.on("message", data => {
-    console.log(`User(${socket.id}): ${data})`);
     // Create/Save message
     Message.create({
       user: `User(${socket.id})`,
@@ -62,11 +46,6 @@ io.on("connect", socket => {
       io.sockets.emit("update", docs)
     })
     .catch(err => console.log(err));
-  });
-
-  //----- Handle disconnect
-  socket.on("disconnect", () => {
-    console.log(`User${socket.id} left`);
   });
 });
 
